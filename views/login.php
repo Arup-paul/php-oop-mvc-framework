1e2798d3d74fd175ef6f0a5478ef3fc47de202aa
Arup paul <aruppaulctg@gmail.com>
2021-03-07 16:53:06 +0600

    Routing controllers views

---
+++ views/login.php
@@ -0,0 +1,22 @@
+<h1 class="text-center">Login</h1>
+
+
+<form action="" method="post">
+        <div class="form-group  ">
+            <label for="name">Subject</label>
+            <input type="text" name="subject" class="form-control" placeholder="Enter your Subject">
+        </div>
+        <div class="form-group  ">
+            <label for="email">Email Address</label>
+            <input type="email" name="email" class="form-control" placeholder="Enter your email">
+        </div>
+
+    <div class="form-group">
+        <label for="name">Body</label>
+        <textarea class="form-control" name="body"></textarea>
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+
+</form>
---
+++ views/register.php
@@ -0,0 +1,35 @@
+<h1 class="text-center">Create an Account</h1>
+
+
+<form action="" method="post">
+    <div class="row">
+        <div class="form-group col-6 ">
+            <label for="name">First Name</label>
+            <input type="text" name="firstname" class="form-control" placeholder="Enter your First Name">
+        </div>
+        <div class="form-group col-6 ">
+            <label for="name">Last Name</label>
+            <input type="text" name="lastname" class="form-control" placeholder="Enter your Last Name">
+        </div>
+
+    </div>
+    <div class="form-group   ">
+        <label for="email">Email Address</label>
+        <input type="email" name="email" class="form-control" placeholder="Enter your email">
+    </div>
+
+    <div class="form-group   ">
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control" placeholder="Enter your password">
+    </div>
+
+
+    <div class="form-group   ">
+        <label for="password">Confirm Password</label>
+        <input type="password" name="passwordConfirm" class="form-control" placeholder=" Confirm password">
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Register</button>
+
+
+</form>
